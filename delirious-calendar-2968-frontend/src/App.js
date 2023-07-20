b4203101746d9052fe1c5208a643282308deb259
Hariom70890 <hariomsunilverma@gmail.com>
2023-07-20 22:03:43 +0530

    home page not done

--- delirious-calendar-2968-frontend/src/App.js
+++ delirious-calendar-2968-frontend/src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import Footer from "./Components/Footer";
 import { AllRoutes } from "./Routes/Allroutes";
+import Navbar from "./Components/Navbar";
 
 
 function App() {
    return (
       <div className="App">
+      <Navbar/>
          <AllRoutes/>
          <Footer />
       </div>
