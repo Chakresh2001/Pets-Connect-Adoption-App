2bfa6ef2e33c2125e9e24bb8355189bd9f7cc9d4
Hariom70890 <hariomsunilverma@gmail.com>
2023-07-22 16:17:23 +0530

    4 static pages are done

--- delirious-calendar-2968-frontend/src/App.js
+++ delirious-calendar-2968-frontend/src/App.js
@@ -7,15 +7,17 @@ import { AboutPetfinder } from "./Pages/AboutPetfinder";
 import { AdoptingPets } from "./Pages/AdoptingPets";
 import { AdminLogin } from "./Admin/AdminLogin";
 import { AdminHomePage } from "./Admin/AdminHomePage";
+import AdoptingDogs from "./Pages/AdoptingDogs";
+import FeedingDog from "./Pages/FeedingDog";
+import AdoptingCats from "./Pages/AdoptionCat";
 
 function App() {
    return (
       <div className="App">
-      {/* <Navbar/>
+       <Navbar/>
 
          <AllRoutes/>
-         <Footer /> */}
-         <AdminHomePage/>
+         <Footer /> 
       </div>
    );
 }
