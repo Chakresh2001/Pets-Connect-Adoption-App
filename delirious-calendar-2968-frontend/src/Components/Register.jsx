43e6fd4571b24585598d82441843ce26066078e1
Chakresh2001 <jha.chakresh2001@gmail.com>
2023-07-23 15:02:44 +0530

    user login, registration, user request pages done do check them

--- delirious-calendar-2968-frontend/src/Components/Register.jsx
+++ delirious-calendar-2968-frontend/src/Components/Register.jsx
@@ -1,14 +1,75 @@
-import React from 'react'
-// import bgImg from '../assets/img1.jpg'
-import { useForm } from 'react-hook-form';
+import React, { useState } from 'react'
 import login_picture from "../Assets/login_picture.jpg"
 import "./Register.css"
+import { useToast } from '@chakra-ui/react';
+import axios from 'axios';
 export default function Register() {
+    
+    const toast = useToast()
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmpwd: '',
+        location: '',
+        gender: '',
+      });
+    
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          [name]: value,
+        }));
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm()
-    const onSubmit = data => console.log(data);
+        if(formData.password!==formData.confirmpwd){
+        return  toast({
+            title: "password and confirm password should be same",
+            status: 'error',
+            duration: 2000,
+            isClosable: true,
+        })
+        }
 
-    // console.log(watch('username'));
+        let obj = {
+            name : formData.name,
+            email : formData.email,
+            password : formData.password,
+            location : formData.location,
+            gender : formData.gender,
+        }
+
+        axios.post("https://shy-erin-perch-kit.cyclic.app/user/register", obj)
+        .then((res)=>{
+            toast({
+                title: `${res.data.message}`,
+                status: "success",
+                isClosable: true,
+            })
+        })
+        .catch((err)=>{
+            if(err.response.status){
+            return  toast({
+                title: err.response.data.error,
+                status: 'error',
+                duration: 2000,
+                isClosable: true,
+            })
+            }
+        })
+
+        
+      };
+
+
+
+
+
+    
     
   return (
    <div className='Regis'> <section>
@@ -17,17 +78,58 @@ export default function Register() {
            <h2 className='Sign'>Sign Up</h2>
            <span className='Sign1'>Register and Enjoy the service</span>
 
-           <form id='form' className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
-               <input type="text" {...register("username")} placeholder='UserName' />
-               <input type="text" {...register("email")} placeholder='Email' />
-               <input type="text" {...register("password")} placeholder='Password' />
-               <input type="text" {...register("confirmpwd")} placeholder='Confirm Password' />
-            
-               <input type="text" {...register("mobile", { required : true, maxLength: 10 })} placeholder='Mobile Number' />
-               {errors.mobile?.type === "required" && "Mobile Number is required"}
-               {errors.mobile?.type === "maxLength" && "Max Length Exceed"}
-               <button className='btn'>Sign Up</button>
-           </form>
+           <form
+        id='form'
+        className='flex flex-col'
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          name="name"
+          value={formData.username}
+          onChange={handleChange}
+          placeholder='UserName'
+        />
+        <input
+          type="text"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder='Email'
+        />
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          placeholder='Password'
+        />
+        <input
+          type="text"
+          name="confirmpwd"
+          value={formData.confirmpwd}
+          onChange={handleChange}
+          placeholder='Confirm Password'
+        />
+        <input
+          type="text"
+          name="location"
+          value={formData.location}
+          onChange={handleChange}
+          placeholder='Location'
+        />
+        <select
+          name="gender"
+          value={formData.gender}
+          onChange={handleChange}
+        >
+          <option value="">Select</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+
+        <button className='btn' type="submit">Sign Up</button>
+      </form>
 
        </div>
        <div className="col-2">
---
+++ delirious-calendar-2968-frontend/src/Pages/AdoptionPage.jsx
@@ -0,0 +1,69 @@
+import { Box, Button, Text } from '@chakra-ui/react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { FaTrash } from 'react-icons/fa6'
+
+export const AdoptionPage = () => {
+
+    const [posts, setPosts] = useState([])
+
+    let getposts = ()=>{
+        let token = JSON.parse(localStorage.getItem("token"))
+        fetch("https://shy-erin-perch-kit.cyclic.app/post", {
+            method:"GET",
+            headers : {
+                "Authorization" : `bearer ${token}`
+            },
+        })
+        .then((res)=>res.json())
+        .then((res)=>{
+            console.log(res)
+            setPosts(res.data)
+        })
+        .catch((err)=>console.log(err))
+    }
+
+    useEffect(()=>{
+        getposts()
+    },[])
+
+    let DeletePost = (id)=>{
+        axios.delete(`https://shy-erin-perch-kit.cyclic.app/post/delete/${id}`).then((res)=>getposts())
+    }
+
+
+  return (
+    <Box padding="40px">
+        {
+            posts.length > 0 ? (
+                <Box display={"grid"} gridTemplateColumns={"1fr 1fr 1fr"} gap="20px" mt="45px" >
+                    {
+                        posts.map((ele)=>(
+                            <Box padding={"20px"} boxShadow={"rgba(0, 0, 0, 0.24) 0px 3px 8px;"}>
+                                <div>
+                                <h1 style={{fontSize:"20px", fontWeight:"bold", marginBottom:"10px"}}>Pets Information</h1>
+                                <img src={ele.pet_image} />
+                                <h2>{ele.pet_name}</h2>
+                                <p> <span style={{fontWeight:"bold"}}>Breed :-</span> {ele.pet_breed}</p>
+                                <p> <span style={{fontWeight:"bold"}}>Age:-</span> {ele.pet_age} years</p>
+                                <p> <span style={{fontWeight:"bold"}}>Location:-</span> {ele.pet_location}</p>
+                                <p><span style={{fontWeight:"bold"}}>Price :-</span>${ele.pet_price}</p>
+                                <Text color={`${ele.status ? "green" : "red"}`} fontWeight={"bold"} fontSize={"24px"}> <span style={{fontWeight:"bold"}}>Status :-</span> {ele.status ? "Approved" : "Pending"}</Text>
+                                <Button mt="15px" colorScheme="red" onClick={()=>DeletePost(ele._id)}> <FaTrash style={{width:"20px", height:"20px"}}/> </Button>
+                            </div>
+                              
+                   
+                               
+                                </Box>
+                        ))
+                    }
+                </Box>
+            ) : (
+                <Box>
+                    <Text>No Adoption Requests Yet</Text>
+                </Box>
+            )
+        }
+    </Box>
+  )
+}
