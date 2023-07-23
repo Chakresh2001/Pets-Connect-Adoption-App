0d039ee3c83335145a48275f2ad2784a1be451b0
Chakresh2001 <jha.chakresh2001@gmail.com>
2023-07-23 12:14:26 +0530

    check

--- delirious-calendar-2968-frontend/src/index.js
+++ delirious-calendar-2968-frontend/src/index.js
@@ -1,7 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import $ from 'jquery';
-import Popper from 'popper.js';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
