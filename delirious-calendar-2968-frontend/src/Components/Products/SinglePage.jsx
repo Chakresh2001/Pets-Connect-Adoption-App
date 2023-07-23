40562e41df520aaa929de5089217abcf1fd5a4b1
Chakresh2001 <jha.chakresh2001@gmail.com>
2023-07-24 03:47:21 +0530

    2 page done

--- delirious-calendar-2968-frontend/src/Components/Products/SinglePage.jsx
+++ delirious-calendar-2968-frontend/src/Components/Products/SinglePage.jsx
@@ -3,12 +3,32 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import  "../../style/Singlepage.css";
 import { Button } from '@chakra-ui/react';
-
+import {  Text, useDisclosure, useToast } from '@chakra-ui/react';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react'
 
 export const SinglePage = () => {
     const [page,setPage]=useState({})
     const {id}=useParams()
 
+    const toast = useToast()
+
+    const OverlayTwo = () => (
+      <ModalOverlay
+        backdropInvert='50%'
+        backdropBlur='5px'
+      />
+    )
+  
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [overlay, setOverlay] = React.useState(<OverlayTwo />)
 
     const singleData=()=>{
         axios.get(`https://shy-erin-perch-kit.cyclic.app/dogs/get/${id}`)
@@ -25,7 +45,45 @@ export const SinglePage = () => {
       
     },[])
 
-    console.log(page)
+    const handleProceed = () => {
+      const object = {
+        pet_name: page.name,
+        pet_breed: page.breed,
+        pet_age: page.age,
+        pet_location: page.location,
+        pet_image: page.image,
+        pet_price: page.price,
+      };
+  
+      console.log(object);
+      console.log(JSON.stringify(object));
+  
+      const token = JSON.parse(localStorage.getItem('token'));
+  
+      // The endpoint URL for the POST request
+      const postEndpoint = 'https://shy-erin-perch-kit.cyclic.app/post/add';
+  
+      axios
+        .post(postEndpoint, object, {
+          headers: {
+            Authorization: `bearer ${token}`,
+          },
+        })
+        .then((response) => {
+          console.log('User data posted successfully:', response.data);
+          toast({
+            title: 'Thank You For Adoption',
+            status: 'success',
+            duration: 2000,
+            isClosable: true,
+          })
+          onClose()
+        })
+        .catch((error) => {
+          console.error('Error posting user data:', error);
+          // Add any error handling logic here (e.g., show an error message).
+        });
+    };
     
 
   return (
@@ -54,7 +112,24 @@ export const SinglePage = () => {
 <br />
 We are reluctantly seeking a new home for {page.name} due to unforeseen circumstances. We kindly request potential adopters to have the time, space, and resources to provide a loving environment. Adoption fees apply to ensure the best match. If you're ready to welcome a loyal and devoted companion into your life, reach out to us at Your Contact Information. Help {page.name} find happiness in a forever home!</p>
       </div>
-      <Button variant={'outline'} >ADOPT</Button>
+      <Button variant={'outline'}  onClick={() => {
+          setOverlay(<OverlayTwo />)
+          onOpen()
+        }} >ADOPT</Button>
+        <Modal isCentered isOpen={isOpen} onClose={onClose}>
+        {overlay}
+        <ModalContent>
+          <ModalHeader color="purple.400">Confirmation</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>Are You Sure You Want To Adopt <span style={{fontWeight:"bold"}}>{page.cat_name}</span></Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={handleProceed} mr="15px" bg="green.400">Proceed</Button>
+            <Button onClick={onClose} bg="red.400">Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
      </div>
     
  </div>
